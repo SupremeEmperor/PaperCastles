3fba3a59967ce994219e2c48d0a692ee5d8060f7
Luke M <lukeam@uci.edu>
2019-01-25 10:48:08 -0800

    Added City.prefab

---
+++ Assets/Scripts/Luke/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    
+    public GameObject[] spawnees;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            Instantiate(spawnees[0], pos, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Luke/IntersectionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntersectionScript : MonoBehaviour {
+
+    public GameObject[] pin;
+    private int path;
+
+    private void Start()
+    {
+        path = 0;
+    }
+
+    private void Update()
+    {
+        path = Random.Range(0, 2);
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        collision.gameObject.GetComponent<EnemyScripts>().ReplacePin(pin[path]);
+        Debug.Log("Trigger");
+    }
+
+}
